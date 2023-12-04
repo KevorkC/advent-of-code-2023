5175d70ad6273dc5c0645625033f10e0db2c5613
Kevork <kevorkchristian@gmail.com>
2023-12-04 01:46:37 +0100

    feat(day01p2): Completed part 2 of day 01

--- src/day01p2.go
+++ src/day01p2.go
@@ -6,7 +6,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode"
+	"strconv"
+	"strings"
 )
 
 type Pair struct {
@@ -15,92 +16,105 @@ type Pair struct {
 }
 
 func firstAndLastInt(line string) Pair {
-	pair := Pair{first: -1, second: -1}
+	var pair Pair = Pair{-1, -1}
+	// fmt.Println("Line:", line)
+	wordToNumber := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
 
-	var pointerLocA int = 0
-	var pointerLocB int = len(line) - 1
+	// List of all numbers between 0 and 9 in both string and int form
+	var numberList []string = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
-	for pointerLocA <= pointerLocB {
-		if pair.first == -1 && unicode.IsDigit(rune(line[pointerLocA])) { // True if pointerLocA is currently pointing to an integer
-			pair.first = int(line[pointerLocA] - '0') // The - '0' is to convert ASCII to number
-		} else if pair.first == -1 {
-			pointerLocA++
+	// *** Finding The First Number From The Front ***
+	// Loop from the front to end, each time adding one more character to the search string
+	for i := 0; i <= len(line); i++ {
+		var assignedFirstNumbner bool = false
+		for _, number := range numberList {
+			// fmt.Println("Searching", line[:i], "for", number)
+			if len(line[:i]) >= len(number) && strings.Contains(line[:i], number) {
+				// Found the number
+				if len(number) > 1 {
+					// Look up in the map first
+					var firstNumber, err = strconv.Atoi(wordToNumber[number])
+					if err != nil {
+						fmt.Println("Error converting word to number:", err)
+					}
+					// fmt.Println("Assigning first number to", firstNumber, "from", number, "in", line)
+					pair.first = firstNumber
+					assignedFirstNumbner = true
+					break
+				} else {
+					var firstNumber, err = strconv.Atoi(number)
+					if err != nil {
+						fmt.Println("Error converting word to number:", err)
+					}
+					// fmt.Println("Assigning first number to", firstNumber, "from", number, "in", line)
+					pair.first = firstNumber
+					assignedFirstNumbner = true
+					break
+				}
+			}
 		}
-
-		if pair.second == -1 && unicode.IsDigit(rune(line[pointerLocB])) { // True if pointerLocA is currently pointing to an integer
-			pair.second = int(line[pointerLocB] - '0') // The - '0' is to convert ASCII to number
-		} else if pair.second == -1 {
-			pointerLocB--
-		}
-
-		if pair.first != -1 && pair.second != -1 {
-			return pair
+		if assignedFirstNumbner {
+			break
 		}
 	}
 
-	fmt.Printf("Warning: Loop exited when either first or second variable in pair is still -1, the line was %s\n", line)
+	// *** Finding The First Number From The Back ***
+	for i := len(line) - 1; i >= 0; i-- {
+		var assignedSecondNumber bool = false
+		for _, number := range numberList {
+			// fmt.Println("Going to check", line[i:], "for", number)
+			if len(line[i:]) >= len(number) && strings.Contains(line[i:], number) {
+				// fmt.Println("Found the number", number, "in", line[i:])
+				// Found the number
+				if len(number) > 1 {
+					// Look up in the map first
+					var secondNumber, err = strconv.Atoi(wordToNumber[number])
+					if err != nil {
+						fmt.Println("Error converting word to number:", err)
+					}
+					// fmt.Println("Assigning second number to", secondNumber, "from", number, "in", line)
+					pair.second = secondNumber
+					assignedSecondNumber = true
+					break
+				} else {
+					var secondNumber, err = strconv.Atoi(number)
+					if err != nil {
+						fmt.Println("Error converting word to number:", err)
+					}
+					// fmt.Println("Assigning second number to", secondNumber, "from", number, "in", line)
+					pair.second = secondNumber
+					assignedSecondNumber = true
+					break
+				}
+			}
+		}
+		if assignedSecondNumber {
+			break
+		}
+	}
+
+	if pair.first == -1 || pair.second == -1 {
+		fmt.Println("ERROR Type -1: First =", pair.first, "Second =", pair.second, "Line =", line)
+	}
 	return pair
 }
 
-func lineLiteralizer(originalLine string) string {
-	// fmt.Println("\nOriginal line:  ", originalLine)
-
-	var replacedFirst string = replaceFirstWord(originalLine)
-	// fmt.Println("Replaced first: ", replacedFirst)
-
-	var replacedBoth string = replaceLastWord(replacedFirst)
-	// fmt.Println("Literal line:   ", replacedBoth)
-
-	return replacedBoth
-}
-
-func replaceFirstWord(originalLine string) string {
-	wordToNumber := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-	}
-
-	for i := 0; i < len(originalLine); i++ {
-		for word, number := range wordToNumber {
-			if len(word) <= len(originalLine[i:]) && originalLine[i:i+len(word)] == word {
-				return originalLine[:i] + number + originalLine[i+len(word):]
-			}
-		}
-	}
-	return originalLine // Return the original string if no replacement is done
-}
-
-func replaceLastWord(originalLine string) string {
-	wordToNumber := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-	}
-
-	for i := len(originalLine); i >= 0; i-- {
-		for word, number := range wordToNumber {
-			if i-len(word) >= 0 && originalLine[i-len(word):i] == word {
-				return originalLine[:i-len(word)] + number + originalLine[i:]
-			}
-		}
-	}
-	return originalLine // Return the original string if no replacement is done
-}
-
 func main() {
+
+	// Tests
+	testAll()
+
+	// Task
 	var filePath string = "files/day01"
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -114,12 +128,7 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println("\nOriginal Line:    ", line)
-		// Convert each word to its number equivalent
-		var literalLine string = lineLiteralizer(line)
-
-		fmt.Println("Literalized line: ", literalLine)
-		pairList = append(pairList, firstAndLastInt(literalLine))
+		pairList = append(pairList, firstAndLastInt(line))
 	}
 
 	var total_sum int = 0
@@ -128,13 +137,47 @@ func main() {
 	}
 
 	fmt.Println(total_sum)
+}
 
-	// const testStr string = "xtwonexonextwonex"
-	const testStr string = "twone"
-	fmt.Println(testStr)
-	var replaced string = replaceFirstWord(testStr)
-	fmt.Println(replaced)
-	replaced = replaceLastWord(replaced)
-	fmt.Println(replaced)
-	fmt.Println(firstAndLastInt(replaced))
+func testAll() {
+	testAdd()
+	testAdd2()
+	testAdd3()
+	testAdd4()
+}
+
+func testAdd() {
+	got := firstAndLastInt("twone")
+	want := Pair{2, 1}
+
+	if got != want {
+		fmt.Println("TEST1 - Got:", got, "Want:", want)
+	}
+}
+
+func testAdd2() {
+	got := firstAndLastInt("xtwonexonextwonex")
+	want := Pair{2, 1}
+
+	if got != want {
+		fmt.Println("TEST2 - Got:", got, "Want:", want)
+	}
+}
+
+func testAdd3() {
+	got := firstAndLastInt("4twone5")
+	want := Pair{4, 5}
+
+	if got != want {
+		fmt.Println("TEST3 - Got:", got, "Want:", want)
+	}
+}
+
+func testAdd4() {
+	got := firstAndLastInt("xsixxseven9eighh")
+	want := Pair{6, 9}
+
+	if got != want {
+		fmt.Println("TEST4 - Got:", got, "Want:", want)
+	}
 }
