38109d9dc272e4e67550261137cfdd30ffe797ed
Kevork <kevorkchristian@gmail.com>
2023-12-09 14:07:42 +0100

    Still working on day05, need to optimize RAM usage

--- src/day05.go
+++ src/day05.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"runtime"
 	"strconv"
@@ -11,36 +12,54 @@ import (
 )
 
 type categoryMap struct {
+	ID                  int
 	mapName             string
-	destinationToSource map[int]int
+	sourceToDestination map[int]int
 }
-
-// seed-to-soil map:
-// 50 98 2
-// 52 50 48
 
 // The worker function that parses the map and creates a map
 func parseAndCreateMapWorker(id int, jobs <-chan []string, results chan<- categoryMap) {
 	for job := range jobs { // job is taken from the pool of jobs
 		var sdm categoryMap
+		// fmt.Println("Worker", id, "is parsing the map", job)
 		sdm.mapName = job[0]
-		sdm.destinationToSource = make(map[int]int)
+
+		switch sdm.mapName {
+		case "seed-to-soil map:":
+			sdm.ID = 1
+		case "soil-to-fertilizer map:":
+			sdm.ID = 2
+		case "fertilizer-to-water map:":
+			sdm.ID = 3
+		case "water-to-light map:":
+			sdm.ID = 4
+		case "light-to-temperature map:":
+			sdm.ID = 5
+		case "temperature-to-humidity map:":
+			sdm.ID = 6
+		case "humidity-to-location map:":
+			sdm.ID = 7
+		default:
+			fmt.Println("Error - Unknown map name:", sdm.mapName)
+		}
+
+		sdm.sourceToDestination = make(map[int]int)
 		for _, line := range job[1:] {
 			var source int
 			var destination int
 			var rangeLength int
 			for i, subStrNumber := range strings.Fields(line) {
 				if i == 0 {
-					source, _ = strconv.Atoi(subStrNumber)
-				} else if i == 1 {
 					destination, _ = strconv.Atoi(subStrNumber)
+				} else if i == 1 {
+					source, _ = strconv.Atoi(subStrNumber)
 				} else if i == 2 {
 					rangeLength, _ = strconv.Atoi(subStrNumber)
 				}
 			}
 			// Now populate the map
 			for i := 0; i < rangeLength; i++ {
-				sdm.destinationToSource[destination+i] = source + i
+				sdm.sourceToDestination[source+i] = destination + i
 			}
 		}
 		results <- sdm
@@ -77,41 +96,36 @@ func main() {
 		seeds = append(seeds, seed)
 	}
 	// fmt.Println("seeds:", seeds)
+	fileLines = fileLines[1:] // Remove the seeds line from the fileLines
 
 	// Defining the job and result channels
 	jobs := make(chan []string, 7)       // Each worker will get a list of strings to parse and create a map from
 	results := make(chan categoryMap, 7) // Each worker will return a map
 
 	// Preparing the jobs for the workers
-	var mapSlice [][]string // The slice of strings that will be sent to the workers
-	var startIndex int      // The start of the current map
-	var endIndex int        // The end of the current map
-	for i, line := range fileLines[1:] {
-		// If the line contains the word "map", it is the start of a new map
-		if strings.Contains(line, "map") {
-			startIndex = i
-		} else if line == "" {
-			endIndex = i
-			mapSlice = append(mapSlice, fileLines[startIndex:endIndex])
+	// var mapSlice [][]string // The slice of strings that will be sent to the workers
+	var currentMapStart int = 0
+	for i, line := range fileLines {
+		if line == "" || i == len(fileLines)-1 {
+			// Send the current map to jobs
+			endIndex := i
+			if line != "" {
+				endIndex = i + 1
+			}
+			if endIndex > currentMapStart {
+				jobs <- fileLines[currentMapStart:endIndex]
+			}
+			currentMapStart = i + 1 // Update the start for the next map
 		}
 	}
-	// for _, mapDefinition := range mapSlice {
-	// 	for _, line := range mapDefinition {
-	// 		fmt.Println(line)
-	// 	}
-	// }
 
-	// Send jobs to the workers.
-	for _, mapDefinition := range mapSlice {
-		jobs <- mapDefinition
-	}
 	close(jobs)
 
 	// Use amount of cores when the work is CPU bound. Otherwise use thread amount if I/O-bound, when involving -
 	// operations like network calls or disk access, where the CPU is often waiting and not continuously processing.
 	var numWorkers int = runtime.NumCPU()
 	var wg sync.WaitGroup
-	fmt.Println("Using", numWorkers, "workers")
+	// fmt.Println("Using", numWorkers, "workers")
 	for w := 1; w <= numWorkers; w++ {
 		wg.Add(1)
 		go func(w int) {
@@ -119,39 +133,70 @@ func main() {
 			parseAndCreateMapWorker(w, jobs, results)
 		}(w)
 	}
-	// /*
-	//  *
-	//  * Seperator
-	//  *
-	//  */
 
 	// Wait for all the workers in the WaitGroup to finish.
+	println("Waiting for workers to finish")
 	wg.Wait()
+	println("Workers finished")
 	close(results)
-
-	// Collect one result
-	// var seedToSoilMap categoryMap = <-results
-	// fmt.Println("seed-to-soil map:", seedToSoilMap.destinationToSource[50])
-	// // for destination, source := range seedToSoilMap.destinationToSource {
-	// // 	fmt.Println(destination, source)
-	// // }
 
 	// Optionally, collect all the results.
 	var maps []categoryMap
 	for r := range results {
 		maps = append(maps, r)
 	}
+	// fmt.Println("LOOK HERE:", maps[0].sourceToDestination[50])
 
-	// Print all the maps' names
-	for _, m := range maps {
-		fmt.Println("Map names:", m.mapName)
+	// Sorting the maps by the lowest ID to the highest ID
+	var sortedMaps []categoryMap
+	for i := 1; i <= 7; i++ {
+		for _, m := range maps {
+			if m.ID == i {
+				sortedMaps = append(sortedMaps, m)
+			}
+		}
 	}
-}
+	// println(sortedMaps[0].mapName)
 
-// type categoryMap struct {
-// 	mapName             string
-// 	destinationToSource map[int]int
-// }
+	// Finding the locaiton for each seed, by tracking through the seed-to-soil map, soil-to-fertilizer map, fertilizer-to-water map,
+	// water-to-light map, light-to-temperature map, temperature-to-humidity map, and humidity-to-location map.
+	// But if the map does not contain a value for the key it is given, then value = key
+	// First we make a list of each seed's location
+	var seedLocations []int = make([]int, len(seeds))
+	// fmt.Println("Seeds:", seeds)
+
+	println("Finding the location for each seed")
+	for seedID, seed := range seeds {
+		var currentValue int = seed
+		for i, m := range sortedMaps {
+			var newValue, ok = m.sourceToDestination[currentValue]
+			// Checking that we're checking the right map, otherwise print an error
+			if m.ID != i+1 {
+				fmt.Println("Error - Wrong map ID:", m.ID, "Expected ID:", i+1)
+			}
+
+			if !ok {
+				// If the map does not contain the key, then the value is the key
+				// fmt.Println("Map", m.mapName, "does not contain the key", currentValue)
+				continue
+			} else {
+				// fmt.Println("Map", m.mapName, "contains the key", currentValue)
+				currentValue = newValue
+			}
+		}
+		seedLocations[seedID] = currentValue
+	}
+	// fmt.Println("Seed:", seeds[0], "Last location", seedLocations[0])
+
+	var lowestLocation int = math.MaxInt32
+	for _, seedLocation := range seedLocations {
+		if seedLocation < lowestLocation {
+			lowestLocation = seedLocation
+		}
+	}
+	fmt.Println("Lowest location:", lowestLocation)
+
+}
 
 /* Input example:
 seeds: 79 14 55 13
@@ -187,4 +232,14 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4
+*/
+
+/* Sorting
+ID 1 = seed-to-soil map:
+ID 2 = soil-to-fertilizer map:
+ID 3 = fertilizer-to-water map:
+ID 4 = water-to-light map:
+ID 5 = light-to-temperature map:
+ID 6 = temperature-to-humidity map:
+ID 7 = humidity-to-location map:
 */
