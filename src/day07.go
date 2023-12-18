8bb36d7a9f160dc78951c390592b30e6f197b4a9
Kevork <kevorkchristian@gmail.com>
2023-12-18 17:04:12 +0100

    feat(day07): Need to compare the hands before assigning rank

--- src/day07.go
+++ src/day07.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -14,9 +15,36 @@ type hand struct {
 	winning int
 }
 
-// func cardsToRank(cards string) int {
+func cardsToRank(cards string) int {
+	// Creating a hashMap to store the count of each card
+	var cardMap = make(map[rune]int)
+	for i, r := range cards {
+		cardMap[r] += i
+	}
 
-// }
+	// Finding how many unique cards there are
+	var uniqueCards int = len(cardMap)
+
+	if uniqueCards == 5 {
+		return 1
+	}
+
+	for _, value := range cardMap {
+		if value == 5 {
+			return 5
+		} else if value == 4 {
+			return 4
+		} else if value == 3 && uniqueCards == 2 {
+			return 3
+		} else if value == 3 && uniqueCards == 3 {
+			return 2
+		} else if value == 2 && uniqueCards == 3 {
+			return 2
+		}
+	}
+
+	return math.MinInt
+}
 
 func stringtoHand(line string) hand {
 	var newHand hand
@@ -27,10 +55,10 @@ func stringtoHand(line string) hand {
 
 	}
 	newHand.bid = bid
-	fmt.Println(newHand.cards, bid)
-	// newHand.rank = cardsToRank(newHand.cards)
+	newHand.rank = cardsToRank(newHand.cards)
 	newHand.winning = newHand.rank * newHand.bid
 
+	fmt.Println(newHand.cards, newHand.bid, newHand.rank)
 	return newHand
 }
 
@@ -48,4 +76,11 @@ func main() {
 		var line string = scanner.Text()
 		hands = append(hands, stringtoHand(line))
 	}
+
+	var totalWinnings = 0
+	for _, hand := range hands {
+		totalWinnings += hand.winning
+	}
+
+	fmt.Println(totalWinnings)
 }
